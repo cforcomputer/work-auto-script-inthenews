1c2ee8037440e6e597e537f8bca016e1db2e0812
cforcomputer <patjobs@pm.me>
2020-06-30 22:15:30 -0700

    Added completed program
    Build not working, working on fix

--- main_format_run.py
+++ main_format_run.py
@@ -1,47 +1,49 @@
 import re  # import regex
 
-counter = 0
-# Read file line by line
-# Every 3 lines, skip 1
 
-with open("input.txt") as f:
-    content = f.readlines()
-    # remove whitespace characters like `\n` at the end of each line
-    content = [x.strip() for x in content]
-    print(content)
+def run_formatter():
+    counter = 0
+    # Read file line by line
+    # Every 3 lines, skip 1
 
-for x in content:
+    with open("input.txt") as f:
+        content = f.readlines()
+        # remove whitespace characters like `\n` at the end of each line
+        content = [x.strip() for x in content]
+        print(content)
 
-    # iterate through list of lines
-    print(x)
-    # If not a blank line, execute
+    for x in content:
 
-    fileWriter = open("output.txt", 'a')
+        # iterate through list of lines
+        print(x)
+        # If not a blank line, execute
 
-    if not counter == 3:
-        # If the start of a paragraph
-        if counter == 0:
-            # Write the first line to be capital
-            fileWriter.write("\n\n<h3>" + x + "</h3>\n")
-            # If not the first paragraph, it is the second paragraph
-            counter += 1
-            continue
+        fileWriter = open("output.txt", 'a')
 
-        elif counter == 1:
-            # perform modifications to each line
-            try:
-                link = re.match(r"[^,]*", x)
-            except AttributeError:
-                link = " [Error]"
+        if not counter == 3:
+            # If the start of a paragraph
+            if counter == 0:
+                # Write the first line to be capital
+                fileWriter.write("\n\n<h3>" + x + "</h3>\n")
+                # If not the first paragraph, it is the second paragraph
+                counter += 1
+                continue
 
-            fileWriter.write("<strong>" + '<a href="https://' + link.group() +
-                             '"</a>' + x.split(",", 1)[1] + "</strong>\n")
-            counter += 1
-            continue
-        # Else it is the third paragraph
+            elif counter == 1:
+                # perform modifications to each line
+                try:
+                    link = re.match(r"[^,]*", x)
+                except AttributeError:
+                    link = " [Error]"
+
+                fileWriter.write("<strong>" + '<a href="https://' + link.group() + '>"' +
+                                 '</a>' + x.split(",", 1)[1] + "</strong>\n")
+                counter += 1
+                continue
+            # Else it is the third paragraph
+            else:
+                fileWriter.write("<p>" + x.split(".", 1)[0] + ".<i>" +
+                                 x.split(".", 1)[1] + "</i>" + "</p>")
+            counter = 3
         else:
-            fileWriter.write("<p>" + x.split(".", 1)[0] + ".<i>" +
-                             x.split(".", 1)[1] + "</i>" + "</p>")
-        counter = 3
-    else:
-        counter = 0
+            counter = 0
---
+++ text_news_converter.py
@@ -0,0 +1,86 @@
+import kivy
+import main_format_run as input_format
+
+from kivy.app import App
+
+# The TextInput widget provides a
+# box for editable plain text
+from kivy.uix.textinput import TextInput
+
+# BoxLayout arranges widgets in either
+# in vertical fashion that
+# is one on top of another or in
+# horizontal fashion that is one after another.
+from kivy.uix.boxlayout import BoxLayout
+
+# Button for submitting the inthenews
+from kivy.uix.button import Button
+
+kivy.require('1.9.0')
+
+
+class Newser(App):
+    def build(self):
+        box_window = BoxLayout(orientation='vertical')
+
+        # Adding the text input
+        text_box = TextInput(font_size=12,
+                             size_hint_y=12,
+                             height=50)
+
+        with open("input.txt", "w+") as f:
+            contents = f.read()
+            text_box.text = contents
+
+        # The format button gui
+        btn = Button(text="Format",
+                     font_size="20sp",
+                     height=30)
+        # The clear button gui
+        btn_clear = Button(text="Clear",
+                           font_size="15sp",
+                           size_hint_y=None,
+                           height=20)
+
+        # The format button action when pressed
+        def format_action(event):
+            print("Button pressed - Formatting")
+            try:
+                input_format.run_formatter()
+                text = open("output.txt", "r")
+                text_box.text = text.read()
+            except SyntaxError:
+                print("Syntax error in main_format")
+
+        # The text box action when typed in
+        def on_text(instance, value):
+            print('The widget', instance, 'has value:', value)
+            text_box_saver = open("input.txt", "w")
+            text_box_saver.write(value)
+
+        # The clear button action when pressed
+        def clear(instance):
+            text_box_saver = open("input.txt", "w")
+            text_output = open("output.txt", "w")
+            text_output.truncate(0)  # clear output.txt
+            text_box_saver.truncate(0)  # clear input.txt
+            text_box.text = ''  # clear the text box in gui
+
+        # Bind button to action
+        # When button is clicked, run the callback and perform action
+        # under def callback(event)
+        btn.bind(on_press=format_action)
+        # Update when new text is entered in the text box (perform text callback)
+        text_box.bind(text=on_text)
+        # Clear the text in the textbox and input.txt when pressed
+        btn_clear.bind(on_press=clear)
+
+        box_window.add_widget(btn)  # Adding button to text box
+        box_window.add_widget(btn_clear)
+        box_window.add_widget(text_box)  # Added Text input box
+
+        return box_window
+
+
+# Running the newser class
+Newser().run()
